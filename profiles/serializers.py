ab9cd51fcaa617c6aad654784c4d82e2ca3f78b9
G-Milzink <G.Milzink@gmail.com>
2023-07-04 19:19:25 +0000

    added login/out views,

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -4,9 +4,17 @@ from .models import Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
 
     class Meta:
         model = Profile
         fields = [
-            'id', 'owner', 'name', 'avatar', 'bio', 'created_on', 'updated_on'
+            'id', 'owner',
+            'name', 'avatar', 'bio',
+            'created_on', 'updated_on',
+            'is_owner'
         ]
