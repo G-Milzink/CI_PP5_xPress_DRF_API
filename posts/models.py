39805329b376c6ef0250be73bc040af8e5520555
G-Milzink <G.Milzink@gmail.com>
2023-07-05 13:20:34 +0000

    add PostSerializer/ fix typo in Post database model

--- posts/models.py
+++ posts/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
 
-class Posts(models.Model):
+class Post(models.Model):
     """
     Post database model.
     """
