d47f68f7cd42d53d9639cd5603f9c6e52f379216
G-Milzink <G.Milzink@gmail.com>
2023-07-05 21:27:11 +0000

    add Comment database model and migrate

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from posts.models import Post
 
-# Create your models here.
+
+class Comment(models.Model):
+    """
+    Comment model,
+    relates to User and Post models.
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    text = models.CharField(max_length=500, blank=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.text
