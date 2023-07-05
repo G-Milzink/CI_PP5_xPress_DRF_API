203ccd5397d4c3dbeec8f072d0ef2aae94b8dd66
G-Milzink <G.Milzink@gmail.com>
2023-07-05 13:06:24 +0000

    cerate post database model and migrate

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Posts(models.Model):
+    """
+    Post database model.
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200, blank=False)
+    text = models.TextField(blank=True)
+    excerpt = models.CharField(max_length=300, blank=True)
+    include_image = models.BooleanField(default=False)
+    image = models.ImageField(
+        upload_to='post_images',
+        default='..xPress/default_post_image'
+    )
+    include_audio = models.BooleanField(default=False)
+    audio = CloudinaryField(
+        resource_type='auto',
+        blank=True
+    )
+    publish = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return f"{self.title} by {self.owner.profile}"
