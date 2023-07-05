1715b0ea05958bb4bee18a1f4683dab3bd69f952
G-Milzink <G.Milzink@gmail.com>
2023-07-05 14:11:53 +0000

    unregister "Groups" section from admin panel

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 from .models import Profile
 
+# remove groups section from admin panel:
+admin.site.unregister(Group)
+
+# add profile section to admin panel:
 admin.site.register(Profile)
