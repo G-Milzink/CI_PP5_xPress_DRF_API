2c8d0289ab50b88b8cfbfbd9808f11a3fe7b0c19
G-Milzink <G.Milzink@gmail.com>
2023-07-06 14:28:48 +0000

    define like database model and migrate

--- likes/models.py
+++ likes/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from posts.models import Post
 
-# Create your models here.
+
+class Like(models.Model):
+    """
+    Like model,
+    relates to User and Post models.
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_on']
+        unique_together = ['owner', 'post']
+
+    def __str__(self):
+        return f"{self.owner} {post}"
