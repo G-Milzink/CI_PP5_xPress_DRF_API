0f587ff503402f8b9b99b41e6439f8d480c08c57
G-Milzink <G.Milzink@gmail.com>
2023-07-07 13:15:20 +0000

    add FollowerDetail view and register urls

--- followers/views.py
+++ followers/views.py
@@ -11,3 +11,9 @@ class FollowerList(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+
+class FollowerDetail(generics.RetrieveDestroyAPIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = FollowerSerializer
+    queryset = Follower.objects.all()
