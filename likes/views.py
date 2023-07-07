3289f62bdd9edff0b39891284de19e8527a6392f
G-Milzink <G.Milzink@gmail.com>
2023-07-07 13:06:28 +0000

    add FollowerList view, fix typo in likes/views.py

--- likes/views.py
+++ likes/views.py
@@ -14,6 +14,6 @@ class LikeList(generics.ListCreateAPIView):
 
 
 class LikeDetail(generics.RetrieveDestroyAPIView):
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [IsOwnerOrReadOnly]
     serializer_class = LikeSerializer
     queryset = Like.objects.all()
