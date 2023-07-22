6e5b91d8f0297a94461f95abd845dab9bb56862f
G-Milzink <G.Milzink@gmail.com>
2023-07-22 18:08:21 +0000

    adjust ordering rules from -created_on to created_on

--- posts/views.py
+++ posts/views.py
@@ -15,7 +15,7 @@ class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.annotate(
         comments_count=Count('comment', distinct=True),
         likes_count=Count('like', distinct=True)
-    ).order_by('-created_on')
+    ).order_by('created_on')
     filter_backends = [
         filters.OrderingFilter,
         filters.SearchFilter,
@@ -24,16 +24,16 @@ class PostList(generics.ListCreateAPIView):
     ordering_fields = [
         'likes_count',
         'comments_count',
-        'likes__created_on',
+        'likes__created_on'
     ]
     search_fields = [
         'owner__username',
-        'title',
+        'title'
     ]
     filterset_fields = [
         'owner__followed__owner__profile',
         'likes__owner__profile',
-        'owner__profile',
+        'owner__profile'
     ]
 
     def perform_create(self, serializer):
@@ -49,4 +49,4 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.annotate(
         comments_count=Count('comment', distinct=True),
         likes_count=Count('like', distinct=True)
-    ).order_by('-created_on')
+    ).order_by('created_on')
