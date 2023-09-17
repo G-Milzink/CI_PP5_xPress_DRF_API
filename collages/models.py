2d8774d806f077fcb00a0cc3692153d8658cf0af
G-Milzink <G.Milzink@gmail.com>
2023-09-17 13:00:36 +0000

    define initial collage db-model (single image)

--- collages/models.py
+++ collages/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Collage(models.Model):
+    """
+    Collage model....
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200, blank=False)
+    collage_description = models.CharField(max_length=300, blank=True)
+    image = models.ImageField(
+        upload_to='collage_images',
+        default='../xPress/default_collage_image',
+        blank=True
+    )
+    publish = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return f"{self.title} by {self.owner}"
